aedf3b4c027c6a82ee3d0d3af8c53d01f57ba05d
Olena457 <olenademianenko42@gmail.com>
2025-12-12 12:30:15 +0200

    installed firebase-admin

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,26 +1,26 @@
-// import * as admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
 
-// const getPrivateKey = (): string | null => {
-//   const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY;
-//   if (!privateKey) {
-//     console.error('FIREBASE_ADMIN_PRIVATE_KEY not found in the invironment!');
-//     return null;
-//   }
-//   return privateKey.replace(/\\n/g, '\n');
-// };
+const getPrivateKey = (): string | null => {
+  const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY;
+  if (!privateKey) {
+    console.error('FIREBASE_ADMIN_PRIVATE_KEY not found in the invironment!');
+    return null;
+  }
+  return privateKey.replace(/\\n/g, '\n');
+};
 
-// const adminConfig: admin.ServiceAccount = {
-//   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-//   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-//   privateKey: getPrivateKey() ?? '',
-// };
+const adminConfig: admin.ServiceAccount = {
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  privateKey: getPrivateKey() ?? '',
+};
 
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(adminConfig),
-//   });
-// }
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+  });
+}
 
-// export const adminAuth = admin.auth();
-// export const adminDb = admin.firestore();
-// export default admin;
+export const adminAuth = admin.auth();
+export const adminDb = admin.firestore();
+export default admin;
